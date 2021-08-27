927c666a6b94be931b73f177df83d3c40ede1acb
Gaurav Singh <gauravkorlasingh@gmail.com>
2021-08-27 13:02:13 +0530

    Endorsement count and explorer overlay for demo: To be deleted

--- identity-web-app/components/SkillCards.js
+++ identity-web-app/components/SkillCards.js
@@ -5,8 +5,43 @@ import Badge from 'react-bootstrap/Badge'
 import styles from '../styles/Home.module.css'
 import CardDropdown from './CardDropdown'
 
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import faker from 'faker'
+
 function SkillCards(props) {
-  const skill = props.skill
+  // TODO: This is a temporary demo for endorsements
+  // If there are no endorsements, show randomly generated data.
+  let totalEndorsements = props.skill.endorsements.length
+  let endorsements = []
+  if (totalEndorsements == 0) {
+    totalEndorsements = Math.floor(Math.random() * 10)
+    // Generate equivalent number of transactions to show in overlay
+    for (let i = 0; i < totalEndorsements; i++) {
+      let endorsement = {
+        transactionId: faker.datatype.uuid(),
+        transactionDate: faker.date.recent()
+      }
+      endorsements.push(endorsement)
+    }
+    console.log(endorsements)
+  }
+
+  // This is the worst piece of code I have ever written
+  const endorsementsToString = (endorsements) => {
+    let endorsementsToString = ''
+    endorsements.forEach(endorsement => {
+      endorsementsToString += endorsement.transactionId
+      endorsementsToString += " "
+      endorsementsToString += "on"
+      endorsementsToString += " "
+      endorsementsToString += endorsement.transactionDate.toLocaleDateString()
+      endorsementsToString += "\n"
+      endorsementsToString += "\n"
+    })
+    return endorsementsToString  
+  }
+
   return (
     <>
       <Col xs={12} md={12} lg={4}>
@@ -16,7 +51,21 @@ function SkillCards(props) {
           className="mt-2 mb-4 p-1 text-center">
           <div className="row">
             <span className="text-left col-8 mt-2">
-             Endorsements: <Badge pill variant="light">{props.skill.endorsements.length}</Badge>
+             Endorsements:&nbsp; 
+             <OverlayTrigger
+                placement="right"
+                overlay={
+                  <Tooltip id='endorsement-tooltip'>
+                    {
+                      (totalEndorsements == 0)
+                        ? <strong> No endorsements </strong>
+                        : <strong>{endorsementsToString(endorsements)}</strong>
+                    } 
+                  </Tooltip>
+                }
+             >
+                <Badge pill variant="light">{totalEndorsements}</Badge>
+             </OverlayTrigger>
             </span>
             <span className="text-right col-4">
              {
