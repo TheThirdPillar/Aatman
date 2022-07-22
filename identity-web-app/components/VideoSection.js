c0871fca88814ac4253ea3d98206232f0007e4f3
Korla <64986263+korlaism@users.noreply.github.com>
2022-07-22 16:01:15 +0530

    Added support for three new video links for dreams, virtues and softskills (#33)

--- identity-web-app/components/VideoSection.js
+++ identity-web-app/components/VideoSection.js
@@ -11,10 +11,15 @@ import { GrClose } from 'react-icons/gr'
 function VideoSection(props) {
   return (
     <>
-        <SectionTitle title="A brief introduction about me" addButtonEnabled={false} />
-        <Row className="d-flex justify-content-end">
-            <GrClose onClick={() => props.closeVideo()} />
-        </Row>
+        <SectionTitle title={props.title} addButtonEnabled={false} />
+        {
+          (props.hasClose)
+            ?
+              <Row className="d-flex justify-content-end">
+                  <GrClose onClick={() => props.closeVideo()} />
+              </Row>
+            : ''
+        }
         <Row className={"justify-content-center m-2 " +  styles.section}>
             <Col xs={12} md={6} lg={6}>
                 <ReactPlayer url={props.url} />
